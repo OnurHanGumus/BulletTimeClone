eef29c7d080c25e5d919a8444fd5cc9261274c15
OnurHG <onurhangumus@gmail.com>
2022-12-16 10:50:05 +0300

    -Simple laser has added

---
+++ Assets/Scripts/Managers/LaserManager.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using Commands;
+using Controllers;
+using Data.UnityObject;
+using Data.ValueObject;
+using Enums;
+using Signals;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Managers
+{
+    public class LaserManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Public Variables
+
+        #endregion
+
+        #region Serialized Variables
+
+        #endregion
+
+        #region Private Variables
+        private LineRenderer _lRenderer;
+        #endregion
+
+        #endregion
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _lRenderer = GetComponent<LineRenderer>();
+        }
+        public PlayerData GetData() => Resources.Load<CD_Player>("Data/CD_Player").Data;
+
+        #region Event Subscription
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+
+
+        }
+
+        private void UnsubscribeEvents()
+        {
+
+        }
+
+
+        private void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+
+        #endregion
+
+        private void Update()
+        {
+            DrawLaser();
+        }
+
+        private void DrawLaser()
+        {
+            _lRenderer.SetPosition(0, transform.position);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                if (hit.collider)
+                {
+                    _lRenderer.SetPosition(1, hit.point);
+                }
+            }
+            else
+            {
+                _lRenderer.SetPosition(1, transform.forward * 5000);
+            }
+
+        }
+
+        private void OnPlay()
+        {
+
+        }
+
+        private void OnResetLevel()
+        {
+
+        }
+    }
+}
